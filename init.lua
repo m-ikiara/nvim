feb137c500250998611ce3c9ba39383edbb338e9
m-ikiara <brnikiara@gmail.com>
2024-03-14 14:32:09 +0300

    Ready. Set. Go!

--- init.lua
+++ init.lua
@@ -1,11 +1,19 @@
-require "overall.options"
-require "overall.buffer"
-require "overall.keymaps"
-require "overall.cmp"
+-- require "overall.script"
+require "overall.autopairs"
+require "overall.buffers"
+require "overall.completion"
 require "overall.plugins"
-require "overall.line"
-require "overall.tree"
-require "overall.term"
-require "overall.colors"
-require "overall.pairs"
+require "overall.gruvbox"
+require "overall.options"
+require "overall.keymaps"
+require "overall.mason"
+require("overall.handlers").setup()
+require "overall.nvim_tree_cfg"
 require "overall.keys"
+require "overall.line"
+require "overall.terminal"
+
+local status_ok, _ = pcall(require, "lspconfig")
+if not status_ok then
+  return
+end
