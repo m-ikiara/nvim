61244524f9d113151a55794e8f960a40e598b748
m-ikiara <brnikiara@gmail.com>
2024-03-14 14:36:57 +0300

    chore: opted for simplicity
    **BREAKING CHANGE** : The removal of requiring Mason will simplify everything
    for everyone.
    
    TODO: Re-implement CMP and LSP for my build.

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
 -- require "overall.script"
-require "overall.autopairs"
-require "overall.buffers"
-require "overall.completion"
+require "overall.pairs"
+require "overall.buffer"
+require "overall.cmp"
 require "overall.plugins"
-require "overall.gruvbox"
+require "overall.colors"
 require "overall.options"
 require "overall.keymaps"
-require "overall.mason"
-require("overall.handlers").setup()
-require "overall.nvim_tree_cfg"
+-- require "overall.mason"
+-- require("overall.handlers").setup()
+require "overall.tree"
 require "overall.keys"
 require "overall.line"
-require "overall.terminal"
+require "overall.term"
 
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
