9a78938942cec2aa83384e6d6dc4d5c7e414856c
m-ikiara <brnikiara@gmail.com>
2024-04-25 10:42:02 +0300

    BREAKING CHANGE: use `expandtab` as well as other niceities

--- lua/overall/options.lua
+++ lua/overall/options.lua
@@ -1,19 +1,20 @@
 local options = {
-	tabstop = 4,
-	shiftwidth = 4,
-	colorcolumn = { 81, 121 },
-	cursorline = true,
-	swapfile = false,
-	number = true,
-	relativenumber = true,
-	showmode = false,
-	ignorecase = true,
-	smartcase = true,
-	fileencoding = "utf-8",
+    tabstop = 4,
+    shiftwidth = 4,
+    expandtab = true,
+    colorcolumn = { 81, 121 },
+    cursorline = true,
+    swapfile = false,
+    number = true,
+    relativenumber = true,
+    showmode = false,
+    ignorecase = true,
+    smartcase = true,
+    fileencoding = "utf-8",
 }
 
 for k, v in pairs(options) do
-	vim.opt[k] = v
+    vim.opt[k] = v
 end
 
 vim.cmd("set listchars=tab:>-,lead:.,precedes:<,extends:>")
